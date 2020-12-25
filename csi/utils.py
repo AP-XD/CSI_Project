e179958d323fd647ae3cdefc01744aaa82950537
APXD-git <63340491+APXD-git@users.noreply.github.com>
2020-12-26 00:56:21 +0530

    fix circular import

--- csi/utils.py
+++ csi/utils.py
@@ -1,16 +1,23 @@
-import sys
-import logging
 import importlib
 from pathlib import Path
 
-def load_plugins(plugin_name):
-    from csi import csi
-    path = Path(f"csi/plugins/{plugin_name}.py")
-    name = "csi.plugins.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["csi.plugins." + plugin_name] = load
-    print("csi has Imported " + plugin_name)
-    mod.csi = csi
+def load_module(plugin_name):
+    if plugin_name.startswith("__"):
+        pass
+    elif plugin_name.endswith("_"):
+        import csi.utils
+        import importlib
+        path = Path(f"csi/plugins/{plugin_name}.py")
+        name = "csi.plugins.{}".format(plugin_name)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("csi has (re)Imported " + plugin_name)
+    else:
+        import csi.utils
+        import importlib
+        path = Path(f"csi/plugins/{plugin_name}.py")
+        name = "csi.plugins.{}".format(plugin_name)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.csi = csi
